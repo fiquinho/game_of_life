aa82c329390904046a68a57caa4116376eefc855
Federico <ficobogado@gmail.com>
2022-03-06 22:53:30 -0300

    Reworked configurations

--- game/menu/menu_utils.py
+++ game/menu/menu_utils.py
@@ -1,41 +1,9 @@
-from enum import Enum
-from pathlib import Path
 from typing import Tuple
 from abc import abstractmethod, ABCMeta
 
 import pygame
 
-from game import Colors
-
-
-class Actions(Enum):
-    PLAY = 0
-    STOP = 1
-    CHANGE_STATE = 2
-    CLEAR = 3
-    NONE = 4
-    LOAD_BOARD = 5
-    SAVE_BOARD = 6
-    MIN_SPEED = 7
-    REDUCE_SPEED = 8
-    INCREASE_SPEED = 9
-    MAX_SPEED = 10
-    OPEN_RULES = 11
-
-
-class MenuLayout:
-    PLAY_BUTTON_SIZE = (70, 70)
-    TEXT_BUTTON_SIZE = (70, 30)
-    HELP_BUTTON_SIZE = (40, 40)
-    BUTTONS_BORDER = 1
-    FONT_FILE = Path("game/assets/SourceCodePro-Regular.ttf")
-    SPEED_DISPLAY_FONT = Path("game/assets/digital-7.mono.ttf")
-    FONT_SIZE = 18
-    BACKGROUND_COLOR = Colors.BLACK
-    MAIN_COLOR = Colors.GREEN
-    SPEED_LABEL_SIZE = (70, 25)
-    SPEED_DISPLAY_SIZE = (70, 40)
-    SPEED_BUTTONS_SIZE = (35, 25)
+from game.configs import MenuLayout, Actions
 
 
 class GameEntity(metaclass=ABCMeta):
